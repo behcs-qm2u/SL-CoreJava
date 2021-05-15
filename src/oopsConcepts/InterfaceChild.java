ad36099f4430e205c74dc3f930acc1ae3ee09933
BehCS <behcsiang@gmail.com>
2021-05-15 05:53:38 +0000

    Interface

---
+++ src/oopsConcepts/InterfaceChild.java
@@ -0,0 +1,5 @@
+package oopsConcepts;
+
+public class InterfaceChild implements InterfaceExample {
+
+}
---
+++ src/oopsConcepts/InterfaceExample.java
@@ -0,0 +1,9 @@
+package oopsConcepts;
+
+public interface InterfaceExample {
+	
+	public void method1() ;
+	public void method2() ;
+	public void method3() ;
+
+}
